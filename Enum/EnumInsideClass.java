2a43001d6bd5b2ded4de862f302eab84c3152af2
ahmed <ahmedsport@hotmail.com>
2021-02-16 13:56:13 +0000

    :ant: 6 :ant:

--- Enum/EnumInsideClass.java
+++ Enum/EnumInsideClass.java
@@ -2,7 +2,7 @@ package Enum;
 
 public class EnumInsideClass 
 {
-    //
+    // create enum inside the class
     enum Flavor
     {
         CHOCOLATE,
@@ -11,22 +11,17 @@ public class EnumInsideClass
     }
     public static void main(String[] args) 
     {
-        // create a value for enum level and called l
-        Level l = Level.LOW;
-        // print the level low 
-        System.out.println(l);
-        
-        // using a switch case with enum
-        switch (l) {
-            case LOW:
-            System.out.println("This is the Low level! ");                
-                break;        
-            case MEDIUM:
-            System.out.println("This is the Medium level! ");                
-                break;        
-            case HIGH:
-            System.out.println("This is the High level! ");                
-                break;        
-        }
+        // create a value for the flavor and called flav 
+        Flavor flav = Flavor.CHOCOLATE;
+
+        // use if statment 
+        if (flav == Flavor.CHOCOLATE) {
+            System.out.println("It's Chocolate! ");            
+        } else if (flav == Flavor.VANILLA) {
+            System.out.println("It's Vanilla! ");            
+        } else if (flav == Flavor.STRAWERRY) {
+            System.out.println("It's Strawerry! ");  
+            
+        }        
     }
 }
--- Enum/Main.java
+++ Enum/Main.java
@@ -1,9 +1,14 @@
 package Enum;
 
+import Enum.EnumInsideClass.Flavor;
+
 public class Main 
 {
     public static void main(String[] args) 
     {
-            
+        // you can call Enum class into main mothed by the below syntax
+        EnumInsideClass insideClass = new EnumInsideClass();
+        System.out.println("this is a test " + " " + Flavor.STRAWERRY);
+
     }    
 }
