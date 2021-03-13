631eab074cb5f1bfe81245dc0e341545acefa993
ahmed <ahmedsport@hotmail.com>
2021-03-13 21:30:44 +0000

    :art: 13 :art:

---
+++ Initialization_Declaration_Assignment/Initialization.java
@@ -0,0 +1,12 @@
+package Initialization_Declaration_Assignment;
+
+public class Initialization 
+{
+    public static void main(String[] args) 
+    {
+        String java = "Is fun"; // initialization   
+        String Python; // declaration 
+        Python = "Django"; // assignment
+
+    }    
+}
---
+++ Logic/Logic.java
@@ -0,0 +1,13 @@
+package Logic;
+
+public class Logic 
+{
+    public static void main(String[] args) 
+    {
+        // || or
+        // && and
+        // ! not
+        boolean a = !(true || false);
+        System.out.println(a);    
+    }    
+}
